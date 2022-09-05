122ccafecf5e0ebd50a89884df363137eb82f268
Chris McGalliard <65915981+mcgalliard@users.noreply.github.com>
2022-09-05 18:48:08 -0400

    Tested User

--- BankingSystem.java
+++ BankingSystem.java
@@ -1,9 +1,9 @@
 //Basic Banking Application
 //Christopher McGalliard 
 
-
 public class BankingSystem {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!"); 
+		BankUser chris = new BankUser("Chris", "McGalliard",1); 
 	}
 }
